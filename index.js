a3be619f2745086b79aa2759470f8034295744fa
Anwesha <anwesha.banerjee.cse23@heritageit.edu.in>
2023-01-19 16:29:09 +0530

    login and register added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const userRoute = require('./routes/user')
+const authRoute = require('./routes/auth')
 
 dotenv.config()
 
@@ -17,6 +18,7 @@ mongoose
 
 app.use(express.json())
 
+app.use('/api/auth', authRoute)
 app.use('/api/users', userRoute)
 app.listen(process.env.PORT || 5000, () => {
   console.log('Backened server running')
